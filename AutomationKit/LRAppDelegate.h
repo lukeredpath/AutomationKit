650ed46f5ae1c65791581a8220a7ac894df8d478
Luke Redpath <luke@lukeredpath.co.uk>
2012-03-25 22:54:40 +0100

    Initial Commit

---
+++ AutomationKit/LRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LRAppDelegate.h
+//  AutomationKit
+//
+//  Created by Luke Redpath on 25/03/2012.
+//  Copyright (c) 2012 LJR Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutomationKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutomationKit
+//
+//  Created by Luke Redpath on 25/03/2012.
+//  Copyright (c) 2012 LJR Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LRAppDelegate class]));
+  }
+}
